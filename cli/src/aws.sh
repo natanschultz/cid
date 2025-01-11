204a20b3aceda9b58b8c569d3f6f2fad771652d3
Natan Schultz <natan@natan.me>
2025-01-11 09:56:22 -0300

    fixing shellcheck errors on cli/src/aws.sh

--- cli/src/aws.sh
+++ cli/src/aws.sh
@@ -1,10 +1,15 @@
+#!/usr/bin/env bash
+#
+# vi: set ff=unix syntax=sh cc=80 ts=2 sw=2 expandtab :
+# shellcheck disable=SC1091,SC1090
+#
 # @description: Abaixo fica a inclusão de todos os tipos de serviços que são utilizados pela AWS.
 # @description: Quando houver necessidade de usar outro serviço dentro da AWS, acrescente aqui o arquivo responsável pelas chamadas.
 # @arg: CLI_FULL_PATH
 
-source ${CLI_FULL_PATH}/src/aws/aws.sh
-source ${CLI_FULL_PATH}/src/aws/api-gateway.sh
-source ${CLI_FULL_PATH}/src/aws/cloudfront.sh
-source ${CLI_FULL_PATH}/src/aws/eks.sh
-source ${CLI_FULL_PATH}/src/aws/lambda.sh
-source ${CLI_FULL_PATH}/src/aws/secrets-manager.sh
+source "${CLI_FULL_PATH}"/src/aws/aws.sh
+source "${CLI_FULL_PATH}"/src/aws/api-gateway.sh
+source "${CLI_FULL_PATH}"/src/aws/cloudfront.sh
+source "${CLI_FULL_PATH}"/src/aws/eks.sh
+source "${CLI_FULL_PATH}"/src/aws/lambda.sh
+source "${CLI_FULL_PATH}"/src/aws/secrets-manager.sh
